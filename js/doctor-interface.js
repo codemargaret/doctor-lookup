ff06327abeda9153b36706d22e87375a072f9ae2
@mdawg85 <margaretshelaghmcgovern@gmail.com>
2017-10-22 18:19:55 -0700

    show contact and other info for returned doctors

--- js/doctor-interface.js
+++ js/doctor-interface.js
@@ -9,7 +9,6 @@ $(document).ready(function() {
     let issue = $('#issue').val();
     let name = $('#name').val();
     let promise = doctor.makePromise(`https://api.betterdoctor.com/2016-03-01/doctors?name=${name}&query=${issue}&location=or-portland&skip=0&limit=10&user_key=5a9aa1f8eda796b71fdded86547df837`);
-    console.log(promise);
     doctor.callApi(promise);
   });
 });
--- js/doctor.js
+++ js/doctor.js
@@ -22,10 +22,11 @@ export class Doctor {
     promise.then(function(response){
       let body = JSON.parse(response);
       body.data.forEach(function(doctor){
-        debugger;
-        $('.showDoctors').append(`<li>${doctor.profile.last_name}, ${doctor.profile.first_name}, ${doctor.practices.website}</li>`);
-        $('.showAddresses').append(`<li>${doctor.practices.visit_address.street}, ${doctor.practices.visit_address.city}, ${doctor.practices.visit_address.state}, ${doctor.practices.visit_address.zip}</li>`);
-        $('.showNewStatus').append(`<li>${doctor.practices.accepts_new_patients}</li>`);
+        $('#showDoctors').append(`<li>Name: ${doctor.profile.last_name}, ${doctor.profile.first_name}</li>`);
+        $('#showDoctors').append(`<li>Number: ${doctor.practices[0].phones.number}</li>`);
+        $('#showDoctors').append(`<li>Website: ${doctor.practices[0].website}</li>`);
+        $('#showAddresses').append(`<li>Address: ${doctor.practices[0].visit_address.street}, ${doctor.practices[0].visit_address.city}, ${doctor.practices[0].visit_address.state}, ${doctor.practices[0].visit_address.zip}</li>`);
+        $('#showNewStatus').append(`<li>Accepts new patients:${doctor.practices[0].accepts_new_patients}</li>`);
       });
       //attempt to show if a search did not return results, but since the loop is breaking, it's not reaching this point
       // if (doctorCount === 0) {
