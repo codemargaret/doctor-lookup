7ed3b517e0a8bd21e9f4c22ceb7eab224b5a3498
@mdawg85 <margaretshelaghmcgovern@gmail.com>
2017-10-22 18:32:27 -0700

    format display to be more readable

--- js/doctor.js
+++ js/doctor.js
@@ -22,11 +22,7 @@ export class Doctor {
     promise.then(function(response){
       let body = JSON.parse(response);
       body.data.forEach(function(doctor){
-        $('#showDoctors').append(`<li>Name: ${doctor.profile.last_name}, ${doctor.profile.first_name}</li>`);
-        $('#showDoctors').append(`<li>Number: ${doctor.practices[0].phones.number}</li>`);
-        $('#showDoctors').append(`<li>Website: ${doctor.practices[0].website}</li>`);
-        $('#showAddresses').append(`<li>Address: ${doctor.practices[0].visit_address.street}, ${doctor.practices[0].visit_address.city}, ${doctor.practices[0].visit_address.state}, ${doctor.practices[0].visit_address.zip}</li>`);
-        $('#showNewStatus').append(`<li>Accepts new patients:${doctor.practices[0].accepts_new_patients}</li>`);
+        $('#showDoctors').append(`<div class="card">Name: ${doctor.profile.last_name}, ${doctor.profile.first_name}, <br> Number: ${doctor.practices[0].phones[0].number}, <br> Website: ${doctor.practices[0].website}, <br> Address: ${doctor.practices[0].visit_address.street}, ${doctor.practices[0].visit_address.city}, ${doctor.practices[0].visit_address.state}, ${doctor.practices[0].visit_address.zip}, <br> Accepts new patients: ${doctor.practices[0].accepts_new_patients}</div>`);
       });
       //attempt to show if a search did not return results, but since the loop is breaking, it's not reaching this point
       // if (doctorCount === 0) {
