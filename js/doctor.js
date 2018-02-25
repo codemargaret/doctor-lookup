cae26f3dfffccc9c877d69ef30e08276da00d706
Margaret Berry <codeberry1@gmail.com>
2018-02-25 12:45:27 -0800

    remove console log and name variables more specifically

--- js/doctor.js
+++ js/doctor.js
@@ -2,10 +2,10 @@ export class Doctor {
   constructor(){
   }
 
-  makePromise(api_key){
+  makePromise(api_endpoint){
     return new Promise(function(resolve, reject) {
       let request = new XMLHttpRequest();
-      let url = api_key;
+      let url = api_endpoint;
       request.onload = function() {
         if (this.status === 200) {
           resolve(request.response);
@@ -25,7 +25,6 @@ export class Doctor {
       body.data.forEach(function(doctor){
         $('#showDoctors').append(`<div class="card">Name: ${doctor.profile.last_name}, ${doctor.profile.first_name} <br> Number: ${doctor.practices[0].phones[0].number} <br> Website: <a href="${doctor.practices[0].website}">${doctor.practices[0].website}</a> Address: ${doctor.practices[0].visit_address.street}, ${doctor.practices[0].visit_address.city}, ${doctor.practices[0].visit_address.state} ${doctor.practices[0].visit_address.zip} <br> Accepts new patients: ${doctor.practices[0].accepts_new_patients}</div>`);
         doctorCount += 1;
-        console.log(doctorCount);
       });
       if (doctorCount === 0) {
         $('.noResults').text("Your search returned no results");
@@ -34,4 +33,4 @@ export class Doctor {
       $('.errors').text(`There was an error processing your request: ${error.message}`);
     });
   }
-} //end Doctor class
+}
