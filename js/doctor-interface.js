0dbec9c5e5f226230cf52e804c7881810a14aa24
Margaret Berry <codeberry1@gmail.com>
2018-02-25 12:47:03 -0800

    clear previous results for each new search, pass in api key as a variable

--- js/doctor-interface.js
+++ js/doctor-interface.js
@@ -5,10 +5,12 @@ $(document).ready(function() {
 
   $('#medical-lookup').submit(function(event) {
     event.preventDefault();
+    $('#showDoctors').text('');
+    $('.noResults').text('');
     let doctor = new Doctor();
     let issue = $('#issue').val();
     let name = $('#name').val();
-    let promise = doctor.makePromise(`https://api.betterdoctor.com/2016-03-01/doctors?name=${name}&query=${issue}&location=or-portland&skip=0&limit=10&user_key=5a9aa1f8eda796b71fdded86547df837`);
+    let promise = doctor.makePromise(`https://api.betterdoctor.com/2016-03-01/doctors?name=${name}&query=${issue}&location=or-portland&skip=0&limit=10&user_key=${apiKey}`);
     doctor.callApi(promise);
   });
 });
